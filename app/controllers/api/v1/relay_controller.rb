0a37ea4f2da522d0ddda4c5022709c02f43d04da
Nick Mark <nmgaultney@gmail.com>
2020-08-23 18:13:37 -0500

    Relay Controls

--- app/controllers/api/v1/relay_controller.rb
+++ app/controllers/api/v1/relay_controller.rb
@@ -8,7 +8,5 @@ class Api::V1::RelayController < ApplicationController
 		Open3.popen2("python3 /home/pi/Projects/RelayHat/RelayHat #{params[:relay]} #{params[:state]}") do |i, o, t|
 			p o.each { |line| Rails.logger.info line }
 		end
-		
-		render :json "Success!"
 	end
 end
