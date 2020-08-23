25eaeadcb283f877669aff000253c271f9e200c6
Nick Mark <nmgaultney@gmail.com>
2020-08-23 18:17:01 -0500

    Relay Controls

--- app/controllers/api/v1/relay_controller.rb
+++ app/controllers/api/v1/relay_controller.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 class Api::V1::RelayController < ApplicationController
 	def controls
-		Open3.popen2("python3 /home/pi/Projects/RelayHat/RelayHat #{params[:relay]} #{params[:state]}") do |i, o, t|
+		Open3.popen2("python3 /home/pi/Projects/RelayHat/RelayHat/relay.py #{params[:relay]} #{params[:state]}") do |i, o, t|
 			p o.each { |line| Rails.logger.info line }
 		end
 	end
